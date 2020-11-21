b1e76be3c4d61834a526d5557ab6046bba187347
KATAR-github <74004757+KATAR-github@users.noreply.github.com>
2020-11-22 00:47:12 +0500

    Source code

---
+++ source/1.2/unit4.pas
@@ -0,0 +1,32 @@
+unit Unit4;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls;
+
+type
+
+  { TForm4 }
+
+  TForm4 = class(TForm)
+    Image1: TImage;
+    Label1: TLabel;
+    Label2: TLabel;
+  private
+
+  public
+
+  end;
+
+var
+  Form4: TForm4;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
